f20df07ba8ef319c3b924ffbd5e6f92118876de9
BMATE <matej.blagsic-ext@hexagon.com>
2025-03-05 13:40:54 +0000

    Format

--- nvim/plugin/git.lua
+++ nvim/plugin/git.lua
@@ -28,27 +28,28 @@ vim.api.nvim_set_hl(0, 'GitSignsTopdeleteNr', { link = 'GitSignsTopdeleteNr' })
 
 -- Configure gitsigns with updated highlight references
 require 'gitsigns'.setup {
-  signs = {
-    add          = { text = '+', },
-    change       = { text = '~', },
-    delete       = { text = '−', },
-    topdelete    = { text = '‾', },
-    changedelete = { text = '≃', },
-  },
-  numhl = true,
-  linehl = false,
-  word_diff = true,
-  watch_gitdir = {
-    interval = 1000, -- Check for git changes every 1 second
-    follow_files = true, -- Follow files in git submodules
-  },
-  on_attach = function(bufnr)
-    local gs = package.loaded.gitsigns
-    -- Key mappings for GitSigns actions
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gd', ':Gitsigns diffthis<CR>', { noremap = true, silent = true })
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gs', ':Gitsigns stage_hunk<CR>', { noremap = true, silent = true })
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gu', ':Gitsigns undo_stage_hunk<CR>', { noremap = true, silent = true })
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gt', ':Gitsigns toggle_signs<CR>', { noremap = true, silent = true })
-  end,
+    signs = {
+        add          = { text = '+', },
+        change       = { text = '~', },
+        delete       = { text = '−', },
+        topdelete    = { text = '‾', },
+        changedelete = { text = '≃', },
+    },
+    numhl = true,
+    linehl = false,
+    word_diff = false,
+    watch_gitdir = {
+        interval = 1000,     -- Check for git changes every 1 second
+        follow_files = true, -- Follow files in git submodules
+    },
+    on_attach = function(bufnr)
+        -- Key mappings for GitSigns actions
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gd', ':Gitsigns diffthis<CR>', { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gs', ':Gitsigns stage_hunk<CR>',
+            { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gu', ':Gitsigns undo_stage_hunk<CR>',
+            { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gt', ':Gitsigns toggle_signs<CR>',
+            { noremap = true, silent = true })
+    end,
 }
-
