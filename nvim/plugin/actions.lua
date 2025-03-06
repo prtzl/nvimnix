932cbc2f357bf8cb33f2a1dda130e70245870549
prtzl <matej.blagsic@protonmail.com>
2025-03-06 23:54:15 +0100

    Move back otherwise it doesn't work, whaaat?

--- nvim/plugin/actions.lua
+++ nvim/plugin/actions.lua
@@ -25,7 +25,6 @@ function vim.g.toggleFormat()
 end
 
 -- Reload file when it has changed
-vim.opt.autoread = true
 vim.api.nvim_create_autocmd({ 'VimEnter', 'FocusGained', 'BufEnter' }, {
     group = vim.api.nvim_create_augroup('ReloadFileOnChange', {}),
     command = 'checktime',
