7bcc392bc837555fc421ae86f5c1130215aa19ff
prtzl <matej.blagsic@protonmail.com>
2025-03-30 15:37:19 +0200

    Update nvim and have it track 24.11

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -1,5 +1,5 @@
 -- Keymaps
-function map(mode, lhs, rhs, opts)
+local function map(mode, lhs, rhs, opts)
     local options = { noremap = true, silent = true }
     if opts then
         options = vim.tbl_extend("force", options, opts)
