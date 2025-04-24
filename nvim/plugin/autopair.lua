5046c3f260e85a3c5ca52a088a5135cdece3a2f8
BMATE <matej.blagsic-ext@hexagon.com>
2025-04-24 10:15:42 +0200

    Use only autopairs plugin

--- nvim/plugin/autopair.lua
+++ nvim/plugin/autopair.lua
@@ -8,16 +8,20 @@ npairs.setup {
     },
     disable_filetype = { "TelescopePrompt", "spectre_panel" },
     fast_wrap = {
-        map = "<M-e>",
-        chars = { "{", "[", "(", '"', "'" },
-        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-        offset = 0,
-        end_key = "$",
-        keys = "qwertyuiopzxcvbnmasdfghjkl",
-        check_comma = "true",
-        highlight = "PmenuSel",
-        highlight_grey = "LineNr",
+        map = '<M-e>',
+        chars = { '{', '[', '(', '"', "'" },
+        pattern = [=[[%'%"%>%]%)%}%,]]=],
+        end_key = '$',
+        before_key = 'h',
+        after_key = 'l',
+        cursor_pos_before = true,
+        keys = 'qwertyuiopzxcvbnmasdfghjkl',
+        manual_position = true,
+        highlight = 'Search',
+        highlight_grey = 'Comment'
     },
+    enable_check_bracket_line = false,
+    ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
 }
 
 -- Hook up autopairs to cmp - when you select, it will put braces around (functions, if, for, etc.)
