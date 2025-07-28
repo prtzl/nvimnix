15e61646a5a8a867bf8a417fa0ca62b8d4073e79
prtzl <matej.blagsic@protonmail.com>
2025-07-28 22:55:03 +0200

    Add mini surround plugin - yey

--- nvim/plugin/autopair.lua
+++ nvim/plugin/autopair.lua
@@ -7,19 +7,6 @@ npairs.setup {
         java = false,
     },
     disable_filetype = { "TelescopePrompt", "spectre_panel" },
-    fast_wrap = {
-        map = '<M-e>',
-        chars = { '{', '[', '(', '"', "'" },
-        pattern = [=[[%'%"%>%]%)%}%,]]=],
-        end_key = '$',
-        before_key = 'h',
-        after_key = 'l',
-        cursor_pos_before = true,
-        keys = 'qwertyuiopzxcvbnmasdfghjkl',
-        manual_position = true,
-        highlight = 'Search',
-        highlight_grey = 'Comment'
-    },
     enable_check_bracket_line = false,
     ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
 }
