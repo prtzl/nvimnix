96d53e131a185d291ecdcc9e527217199344fe09
prtzl <matej.blagsic@protonmail.com>
2025-07-28 22:55:03 +0200

    Add rainbow delims config

---
+++ nvim/plugin/rainbow.lua
@@ -0,0 +1,20 @@
+-- Optional config for rainbow-delimiters
+vim.g.rainbow_delimiters = {
+    strategy = {
+        [''] = require('rainbow-delimiters.strategy.global'),
+        vim = require('rainbow-delimiters.strategy.local'),
+    },
+    query = {
+        [''] = 'rainbow-delimiters',
+        lua = 'rainbow-blocks',
+    },
+    highlight = {
+        'RainbowDelimiterRed',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterViolet',
+        'RainbowDelimiterCyan',
+    },
+}
