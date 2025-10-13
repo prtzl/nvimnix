3c8ee3eb8108886e6c5aee6c0c93e8f36b1c883d
prtzl <matej.blagsic@protonmail.com>
2025-10-13 23:30:21 +0200

    Mess about because its been working too well ...

--- nvim/plugin/buffers.lua
+++ nvim/plugin/buffers.lua
@@ -4,7 +4,7 @@ local mru_buffers = {}
 local current_index = 1
 local cycling = false
 
-function dump(o)
+local function dump(o)
     if type(o) == 'table' then
         local s = '{ '
         for k, v in pairs(o) do
