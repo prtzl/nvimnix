d587610edae83308dd9100e8d22f2a01aceec5fd
prtzl <matej.blagsic@protonmail.com>
2026-02-11 16:13:46 +0100

    Full path in lualine header

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -34,7 +34,7 @@ require('lualine').setup {
     winbar = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { 'filename' },
+        lualine_c = { '%f' },
         lualine_x = {},
         lualine_y = {},
         lualine_z = {}
